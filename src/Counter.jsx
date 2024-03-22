984591a6b723f4f46e34d29cd59ab49597cda155
George <g.m.6@live.com>
2024-03-23 00:38:37 +0200

    Removing working

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -9,16 +9,18 @@ const Counter = ({ depth = 0, initialCount = -1 }) => {
     setChildren([...children, <Counter key={children.length} depth={depth + 1} initialCount={children.length === 0 ? -1 : count} />]);
   };
 
+  const removeChild = () => {
+    setChildren([]);
+  };
+
   return (
     <div style={{ paddingLeft: `${depth * 20}px` }}>
       <span>{initialCount}</span>
       <button onClick={addChild}>+</button>
-      <button onClick={() => setCount(count - 1)}>-</button>
+      <button onClick={removeChild}>-</button>
       {children}
     </div>
   );
 };
 
-export default function App() {
-  return <Counter />;
-}
+export default Counter;
